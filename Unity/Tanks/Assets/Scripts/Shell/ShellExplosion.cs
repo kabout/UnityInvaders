adfc40e56eca7b715914983494f58a08475bf77d
Diego Sanchez <dsanchezd@externo.wke.es>
2016-08-12 14:56:52 +0200

    Añadido Proyecto de Prueba Tanks

---
+++ Unity/Tanks/Assets/Scripts/Shell/ShellExplosion.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class ShellExplosion : MonoBehaviour
+{
+    public LayerMask TankMask;
+    public ParticleSystem ExplosionParticles;       
+    public AudioSource ExplosionAudio;              
+    public float MaxDamage = 100f;                  
+    public float ExplosionForce = 1000f;            
+    public float MaxLifeTime = 2f;                  
+    public float ExplosionRadius = 5f;              
+
+
+    private void Start()
+    {
+        Destroy(gameObject, MaxLifeTime);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Find all the tanks in an area around the shell and damage them.
+        Collider[] colliders = Physics.OverlapSphere(transform.position, ExplosionRadius, TankMask);
+
+        foreach(Collider collider in colliders)
+        {
+            Rigidbody targetRigidBody = collider.GetComponent<Rigidbody>();
+
+            if (targetRigidBody == null)
+                continue;
+
+            targetRigidBody.AddExplosionForce(ExplosionForce, transform.position, ExplosionRadius);
+            TankHealth targetHealth = collider.GetComponent<TankHealth>();
+
+            if (targetHealth == null)
+                continue;
+
+            float damage = CalculateDamage(targetRigidBody.position);
+            targetHealth.TakeDamage(damage);
+        }
+        ExplosionParticles.transform.SetParent(null);
+        ExplosionParticles.Play();
+        ExplosionAudio.Play();
+
+        Destroy(gameObject);
+        Destroy(ExplosionParticles, ExplosionParticles.duration);
+    }
+
+
+    private float CalculateDamage(Vector3 targetPosition)
+    {
+        // Calculate the amount of damage a target should take based on it's position.
+        Vector3 explosionToTarget = targetPosition - transform.position;
+        float explosionDistance = Mathf.Min(explosionToTarget.magnitude, ExplosionRadius);
+        float relativeDistance = (ExplosionRadius - explosionDistance) / ExplosionRadius;
+
+        return relativeDistance * MaxDamage;
+    }
+}
---
+++ Unity/Tanks/Assets/Scripts/Tank/TankHealth.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TankHealth : MonoBehaviour
+{
+    public float StartingHealth = 100f;          
+    public Slider Slider;                        
+    public Image FillImage;                      
+    public Color FullHealthColor = Color.green;  
+    public Color ZeroHealthColor = Color.red;    
+    public GameObject ExplosionPrefab;    
+    
+    private AudioSource ExplosionAudio;          
+    private ParticleSystem ExplosionParticles;   
+    private float CurrentHealth;  
+    private bool Dead;            
+
+
+    private void Awake()
+    {
+        ExplosionParticles = Instantiate(ExplosionPrefab).GetComponent<ParticleSystem>();
+        ExplosionAudio = ExplosionParticles.GetComponent<AudioSource>();
+
+        ExplosionParticles.gameObject.SetActive(false);
+    }
+
+
+    private void OnEnable()
+    {
+        CurrentHealth = StartingHealth;
+        Dead = false;
+
+        SetHealthUI();
+    }
+    
+
+    public void TakeDamage(float amount)
+    {
+        // Adjust the tank's current health, update the UI based on the new health and check whether or not the tank is dead.
+        CurrentHealth -= amount;
+
+        SetHealthUI();
+
+        if (CurrentHealth <= 0f && !Dead)
+            OnDeath();
+    }
+
+
+    private void SetHealthUI()
+    {
+        // Adjust the value and colour of the slider.
+        Slider.value = CurrentHealth;
+        FillImage.color = Color.Lerp(ZeroHealthColor, FullHealthColor, StartingHealth / CurrentHealth);
+    }
+
+
+    private void OnDeath()
+    {
+        // Play the effects for the death of the tank and deactivate it.
+        Dead = true;
+
+        ExplosionParticles.transform.position = transform.position;
+        ExplosionParticles.gameObject.SetActive(true);
+        ExplosionParticles.Play();
+        ExplosionAudio.Play();
+
+        gameObject.SetActive(false);
+    }
+}
