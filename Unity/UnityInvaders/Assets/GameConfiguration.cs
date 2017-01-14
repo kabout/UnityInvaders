0dae5e9543920f5aae27cf044fc78fbfb210b164
Diego <diego.sanchezdiaz@gmail.com>
2017-01-14 19:04:51 +0100

    Avanzando en los menus

---
+++ Unity/UnityInvaders/Assets/GameConfiguration.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameConfiguration : MonoBehaviour
+{
+    public enum GameVelocity
+    {
+        VerySlow = 0,
+        Slow = 1,
+        Normal = 2,
+        Fast = 3,
+        VeryFast = 4
+    }
+
+    public static GameConfiguration gameConfiguration;
+
+    void Awake()
+    {
+        if (gameConfiguration == null)
+        {
+            gameConfiguration = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (gameConfiguration != this)
+            Destroy(gameObject);
+
+        StrategyLocationDefensesDllPath = Application.dataPath + @"/../Lib/StrategyLocationDefenses.dll";
+        StrategySelectionDefensesDllPath = Application.dataPath + @"/../Lib/StrategySelectionDefenses.dll";
+        StrategyAttackAliensDllPath = Application.dataPath + @"/../Lib/StrategyAttackAliens.dll";
+        StrategyDefenderAliensDllPath = Application.dataPath + @"/../Lib/StrategyDefenderAliens.dll";
+
+        SizeMap = 300;
+        CellMap = 10;
+        NumUcos = int.MaxValue;
+        NumUcosPerSecond = 2f;
+        // 5 minutos
+        MaxDurationBattleInSeconds = 300;
+        NumObjects = int.MaxValue;
+        NumDefenses = int.MaxValue;
+        Velocity = GameVelocity.Normal;
+        DensityDefenses = 0.50f;
+        DensityObstacles = 0.50f;
+        NumAses = 1000;
+    }
+
+    public string StrategyLocationDefensesDllPath { get; set; }
+    public string StrategySelectionDefensesDllPath { get; set; }
+    public string StrategyAttackAliensDllPath { get; set; }
+    public string StrategyDefenderAliensDllPath { get; set; }
+
+    public int SizeMap { get; set; }
+    public int CellMap { get; set; }
+    public int NumUcos { get; set; }
+    public float NumUcosPerSecond { get; set; }
+    public int MaxDurationBattleInSeconds { get; set; }
+    public int NumObjects { get; set; }
+    public int NumDefenses { get; set; }
+    public GameVelocity Velocity { get; set; }
+    public float DensityObstacles { get; set; }
+    public float DensityDefenses { get; set; }
+    public int NumAses { get; set; }
+}
--- Unity/UnityInvaders/Assets/MenuScript.cs
+++ Unity/UnityInvaders/Assets/MenuScript.cs
@@ -3,54 +3,145 @@ using System.Collections;
 using UnityEditor;
 using System;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
+    #region Fields
+
     public Canvas MainMenu;
-    public Canvas TeamMenu;
-    public Canvas DllMenu;
+    public Canvas GameMenu;
+
+    public GameConfiguration gameConfiguration;
+
+    public Toggle CbStrategyLocation;
+    public Button BtStrategyLocation;
+    private string pathStrategyLocationDll;
+
+    public Toggle CbStrategySelection;
+    public Button BtStrategySelection;
+    private string pathStrategySelectionDll;
+
+    public Toggle CbStrategyAttack;
+    public Button BtStrategyAttack;
+    private string pathStrategyAttackDll;
+
+    public Toggle CbStrategyDefender;
+    public Button BtStrategyDefender;
+    private string pathStrategyDefenderDll;
+
+    #endregion
 
     public void Start()
     {
         ChangeToMainMenu();
     }
 
-    public void ChangeToTeamMenu()
+    #region Methods of MainMenu
+
+    public void ChangeToGameMenu(string type)
     {
         MainMenu.gameObject.SetActive(false);
-        DllMenu.gameObject.SetActive(false);
-        TeamMenu.gameObject.SetActive(true);
-    }
-
-    public void ChangeToMainMenu()
-    {
-        TeamMenu.gameObject.SetActive(false);
-        DllMenu.gameObject.SetActive(false);
-        MainMenu.gameObject.SetActive(true);
-    }
-
-    public void ChangeToDllMenu(string type)
-    {
-        switch(type)
-        {
-            case "StrategyDefenseLocation": break;
-            case "StrategyDefenseSelection": break;
-            case "StrategyAlienAttack": break;
-        }
-
-        TeamMenu.gameObject.SetActive(false);
-        MainMenu.gameObject.SetActive(false);
-        DllMenu.gameObject.SetActive(true);
-    }
-
-    public void SelectDll()
-    {
-        string path = EditorUtility.OpenFilePanel("Select Dll", Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "dll");
-        DllMenu.GetComponentInChildren<InputField>().text = path;
+        GameMenu.gameObject.SetActive(true);
     }
 
     public void Exit()
     {
         Application.Quit();
     }
+
+    #endregion
+
+    #region Methods of GameMenu
+
+    public void StartGame()
+    {
+        if (!string.IsNullOrEmpty(pathStrategyLocationDll))
+            gameConfiguration.StrategyLocationDefensesDllPath = pathStrategyLocationDll;
+
+        if (!string.IsNullOrEmpty(pathStrategySelectionDll))
+            gameConfiguration.StrategySelectionDefensesDllPath = pathStrategySelectionDll;
+
+        if (!string.IsNullOrEmpty(pathStrategyAttackDll))
+            gameConfiguration.StrategyAttackAliensDllPath = pathStrategyAttackDll;
+
+        if (!string.IsNullOrEmpty(pathStrategyDefenderDll))
+            gameConfiguration.StrategyDefenderAliensDllPath = pathStrategyDefenderDll;
+
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void ChangeToMainMenu()
+    {
+        GameMenu.gameObject.SetActive(false);
+        MainMenu.gameObject.SetActive(true);
+    }
+
+    public void SelectStrategyLocationDll()
+    {
+        SelectStrategyDll(BtStrategyLocation, ref pathStrategyLocationDll);
+    }
+
+    public void SelectStrategySelectionDll()
+    {
+        SelectStrategyDll(BtStrategySelection, ref pathStrategySelectionDll);
+    }
+
+    public void SelectStrategyAttackDll()
+    {
+        SelectStrategyDll(BtStrategyAttack, ref pathStrategyAttackDll);
+    }
+
+    public void SelectStrategyDefenderDll()
+    {
+        SelectStrategyDll(BtStrategyDefender, ref pathStrategyDefenderDll);
+    }
+
+    private void SelectStrategyDll(Button button, ref string path)
+    {
+        string initialPath = string.IsNullOrEmpty(path) ?
+            Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) :
+            path;
+
+        string newPath = EditorUtility.OpenFilePanel("Select Dll", initialPath, "dll");
+
+        if (newPath == string.Empty)
+            return;
+
+        button.GetComponentInChildren<Text>().text = "Change Dll";
+        path = newPath;
+    }
+
+    public void AutomaticStrategyLocation()
+    {
+        AutomaticStrategy(BtStrategyLocation, CbStrategyLocation, ref pathStrategyLocationDll);
+    }
+
+    public void AutomaticStrategySelection()
+    {
+        AutomaticStrategy(BtStrategySelection, CbStrategySelection, ref pathStrategySelectionDll);
+    }
+
+    public void AutomaticStrategyAttack()
+    {
+        AutomaticStrategy(BtStrategyAttack, CbStrategyAttack, ref pathStrategyAttackDll);
+    }
+
+    public void AutomaticStrategyDefender()
+    {
+        AutomaticStrategy(BtStrategyDefender, CbStrategyDefender, ref pathStrategyDefenderDll);
+    }
+
+    private void AutomaticStrategy(Button button, Toggle toggle, ref string path)
+    {
+        button.interactable = !toggle.isOn;
+
+        if (toggle.isOn)
+        {
+            button.GetComponentInChildren<Text>().text = "Select Dll";
+            path = string.Empty;
+        }
+    }
+
+    #endregion
 }
